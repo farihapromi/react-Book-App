517cac1a715bf495960acf654777f2ef3f324c0a
Fariha Promi <farihapromi2611@gmail.com>
2025-05-18 21:38:44 +0600

    Add bookDetails

--- src/components/BookDetails/BookDetail.jsx
+++ src/components/BookDetails/BookDetail.jsx
@@ -4,11 +4,44 @@ import { useLoaderData, useParams } from 'react-router-dom'
 const BookDetail = () => {
     const {bookId}=useParams()
     const id=parseInt(bookId)
-    const {data}=useLoaderData()
+    const data=useLoaderData()
     const book=data.find(book=>book.bookId===id)
+    console.log(book)
+    const{bookId:curentBookId,image,bookName,category,author,review,tags}=book
   return (
+  <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row gap-8">
+    <img
+      src={image}
+      className="h-[326px]" />
     <div>
-        <h2>Book details:</h2>
+
+        <h1 className='font-bold text-5xl'>{bookName}</h1>
+        <h3>By:{author}</h3>
+           <div className="divider"></div>
+           <h3>{category}</h3>
+           <p className="text-xl"><span className='font-bold'>Review: </span>{review}</p>
+         
+         <div className="flex items-center gap-2 justify-start m-4">
+  <p className="font-bold">Tag:</p>
+  {tags.map((tag, idx) => (
+    <button key={idx} className="btn text-green-500 font-bold">
+      {tag}
+    </button>
+  ))}
+    <div className="divider"></div>
+</div>
+
+     
+    <div className='flex gap-4'>
+            <button className='btn btn-outline btn-accent'>Read</button>
+        <button className='btn btn-accent'>WishList</button>
+
+        </div>
+    </div>
+  </div>
+
+        
       
     </div>
   )
