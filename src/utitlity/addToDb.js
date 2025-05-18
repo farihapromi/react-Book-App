d4a7cee8138ac1a91fb7a6e747e89452bff25543
Fariha Promi <farihapromi2611@gmail.com>
2025-05-18 22:26:20 +0600

    Add localstorage for storing items

--- src/utitlity/addToDb.js
+++ src/utitlity/addToDb.js
@@ -1,9 +1,20 @@
-const getStoredReadList = () => {};
+const getStoredReadList = () => {
+  const storedListStr = localStorage.get('read-list');
+  if (storedListStr) {
+    const storedList = JSON.parse(storedListStr);
+    return storedList;
+  } else {
+    return [];
+  }
+};
 const addToStoredReadList = (id) => {
   const storedList = getStoredReadList();
   if (storedList.includes(id)) {
     //already exist
+    console.log(id, 'Already exists');
   } else {
     storedList.push(id);
+    const storedListstr = JSON.stringify(storedList);
+    localStorage.setItem('read-list', storedListstr);
   }
 };
